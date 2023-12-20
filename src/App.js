90660a4e12d7acbc3c5aaea745f8015d90a0748a
elimarayag <marayagelisha@gmail.com>
2023-12-20 18:39:49 +0800

    created app js and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+
+  const setToZero = () => {
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="CenterBox">
+      <div className="Box">
+        <button onClick={increaseCount} className="Button">Increase</button>
+        <button onClick={decreaseCount} className="Button">Decrease</button>
+        <button onClick={setToZero} className="Button">Set to Zero</button>
+        <h1>{count}</h1>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
